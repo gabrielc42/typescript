58fcba1143d5246c158b3990e615e1b574daf98d
gabrielc42 <clark.gabe97@gmail.com>
2022-12-06 11:35:10 -0600

    fetchJSON, interface, type annotations

---
+++ fetchJSON/index.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then(response => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  logTodo(id, completed, title);
+});
+
+const logTodo = (id: number, completed: boolean, title: string) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished?: ${completed}
+  `);
+};
