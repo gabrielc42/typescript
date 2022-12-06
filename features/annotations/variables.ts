4cd4ef165ae8ba586b59b7a9483f9be33eecdc66
gabrielc42 <clark.gabe97@gmail.com>
2022-12-06 12:09:06 -0600

    typescript features, types, classes, object literals

---
+++ features/annotations/variables.ts
@@ -0,0 +1,26 @@
+let apples: number = 5;
+let spped: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built in objects
+let now: Date = new Date();
+
+// array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1,2,3];
+let truths: boolean[] = [true, true, false];
+
+//classes
+class Car {
+
+}
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
