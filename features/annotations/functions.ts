d413da755e40223c8ac6a3def9423cb606c7d050
gabrielc42 <clark.gabe97@gmail.com>
2022-12-09 12:16:29 -0600

    inference around functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,7 @@
-const add = (a:number,b:number): number => {
-  return a+b;
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
 };
