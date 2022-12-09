c458b125fd9c88231cc046ae8898c50b8aa91c23
gabrielc42 <clark.gabe97@gmail.com>
2022-12-09 17:10:46 -0600

    cannot differentiate string | string when type init, tuples lesson

---
+++ features/tuples.ts
@@ -0,0 +1,20 @@
+const drink = {
+  color: 'clear',
+  carbonated: false,
+  sugar: 0
+};
+
+type Drink = [string, boolean, number];
+
+const water: Drink 
+= ['clear', false, 0];
+
+const tea: Drink 
+= ['green', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+}
