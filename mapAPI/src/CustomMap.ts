a4fd77ff1ed5845dc40e14b3811d24ca683cc0ca
gabrielc42 <clark.gabe97@gmail.com>
2022-12-14 11:38:56 -0600

    mappable interface

---
+++ mapAPI/src/CustomMap.ts
@@ -0,0 +1,34 @@
+import { User } from './User';
+import { Company } from './Company';
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map((document.getElementById(divId) as HTMLElement), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+  }
+  // addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng
+  //     }
+  //   });
+  }
--- mapAPI/src/index.ts
+++ mapAPI/src/index.ts
@@ -1,19 +1,15 @@
 /// <reference types="@types/google.maps" />
 
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-// const user = new User();
-
+const user = new User();
 // console.log(user);
-
-// const company = new Company();
+const company = new Company();
 // console.log(company);
 
-new google.maps.Map((document.getElementById('map') as HTMLElement), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+const customMap = new CustomMap('map');
+
+// customMap.addUserMarker(user);
+// customMap.addCompanyMarker(company);
